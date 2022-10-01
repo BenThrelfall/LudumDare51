e60ba759b63c6d57535244465d8256f786546f0f
Benjamin Threlfall <threlfall.b@gmail.com>
2022-10-01 15:23:17 +0100

    Created basic health script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public float maxHealth;
+    private float currentHealth;
+    public float CurrentHealth { get {return currentHealth; } private set { currentHealth = Mathf.Min(value, maxHealth); } }
+
+    public void TakeDamage(float amount) {
+        CurrentHealth -= amount;
+        if (CurrentHealth <= 0) Die();
+    }
+
+
+    public void GetHealed(float amount) {
+        CurrentHealth += amount;
+    }
+
+
+    /// <summary>
+    /// Sets health ignoring any effects
+    /// </summary>
+    /// <param name="amount"></param>
+    public void SetHealth(float amount) {
+        CurrentHealth = amount;
+    }
+
+    private void Die() {
+        Destroy(gameObject); //TEMP
+    }
+
+}
