a90bf86cfed3edbcf882300b438b2e66e0e47d3c
Benjamin Threlfall <threlfall.b@gmail.com>
2022-10-01 22:40:56 +0100

    Added more cards

---
+++ Assets/Scripts/AbilityEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class AbilityEffect {
+
+    public virtual void DoAbility() { return; }
+
+}
