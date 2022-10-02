79dfeb1e11a261b2268f39beaf182f3c5c4c87dd
Benjamin Threlfall <threlfall.b@gmail.com>
2022-10-02 22:56:59 +0100

    Added postion based z axis position

--- Assets/DynamicZPosition.cs
+++ Assets/DynamicZPosition.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DynamicZPosition : MonoBehaviour {
 
     private void LateUpdate() {
-        transform.position = new Vector3(transform.position.x, transform.position.y, 20 - transform.position.y * 0.1f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y * 0.1f);
     }
 
 }
--- Assets/Scripts/StaticZPosition.cs
+++ Assets/Scripts/StaticZPosition.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class StaticZPosition : MonoBehaviour {
 
     private void Start() {
-        transform.position = new Vector3(transform.position.x, transform.position.y, 20 - transform.position.y * 0.1f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y * 0.1f);
     }
 
 }
