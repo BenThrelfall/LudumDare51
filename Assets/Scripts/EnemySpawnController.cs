96c0a643952138b752a38531f8b21fd8a32ea5fc
Benjamin Threlfall <threlfall.b@gmail.com>
2022-10-03 00:51:51 +0100

    Added many graphics and UI elements

--- Assets/Scripts/EnemySpawnController.cs
+++ Assets/Scripts/EnemySpawnController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class EnemySpawnController : MonoBehaviour {
 
     Transform playerTrans;
-    public float spawnCoolDown = 5f;
+    public float spawnCoolDown = 4f;
 
-    public GameObject enemyPrefab;
+    public GameObject RobotOne;
+    public GameObject RobotTwo;
+    public GameObject BioOne;
+
 
     private void Start() {
         playerTrans = GlobalPlayer.instance.transform;
@@ -17,10 +20,22 @@ public class EnemySpawnController : MonoBehaviour {
     IEnumerator SpawningRoutine() {
 
         while (true) {
-            yield return new WaitForSeconds(spawnCoolDown);
-            spawnCoolDown = Mathf.Max(spawnCoolDown - 0.1f, 1f);
+            spawnCoolDown = Mathf.Max(spawnCoolDown - 0.1f, 0.3f);
             Vector2 spawnPosition = Random.insideUnitCircle.normalized * 10;
-            Instantiate(enemyPrefab, playerTrans.position + (Vector3)spawnPosition, Quaternion.identity);
+            int spawn = Random.Range(0, 3);
+
+            if (spawn == 0) {
+                Instantiate(RobotOne, playerTrans.position + (Vector3)spawnPosition, Quaternion.identity);
+
+            }
+            else if (spawn == 1) { 
+                Instantiate(RobotTwo, playerTrans.position + (Vector3)spawnPosition, Quaternion.identity);
+            }
+            else {
+                Instantiate(BioOne, playerTrans.position + (Vector3)spawnPosition, Quaternion.identity);
+            }
+
+            yield return new WaitForSeconds(spawnCoolDown);
         }
 
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,24 @@ public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] Rigidbody2D rbod;
 
+    [SerializeField] Animator animator;
+
+    [SerializeField] SpriteRenderer spriteRenderer;
+
     // Update is called once per frame
     void FixedUpdate() {
 
         Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         rbod.velocity = movementVector.normalized * GlobalPlayer.instance.playerMoveSpeed;
+        animator.SetFloat("SquareMoveSpeed", rbod.velocity.sqrMagnitude);
+
+        if (rbod.velocity.x < 0) { 
+            spriteRenderer.flipX = true;
+        }
+        else {
+            spriteRenderer.flipX = false;
+        }
+
     }
 }
